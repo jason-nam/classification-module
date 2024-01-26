bed69189610640cd29ee4759c25627c1d4a49611
namy2 <namy2@mcmaster.ca>
2024-01-26 14:28:28 -0500

    added more category urls to web crawler

--- src/web_crawler.py
+++ src/web_crawler.py
@@ -14,6 +14,8 @@ def get_all_links(driver, url):
     anchors = driver.find_elements(By.TAG_NAME, "a")
     links = set()
 
+    print(f'Crawling through {url} ...')
+
     # Re-fetch the anchor elements by their index to avoid StaleElementReferenceException
     for i in range(len(anchors)):
         try:
@@ -30,6 +32,11 @@ def get_all_links(driver, url):
         except StaleElementReferenceException:
             # If the element has gone stale, skip and move to the next
             continue
+    
+    if not links:
+        raise Exception(f'The web-crawler failed to crawl through {url}')
+    
+    print(f'The web-crawler successfuly crawled through {url}')
 
     return links
 
@@ -40,8 +47,9 @@ service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=options)
 
 try:
-    base_url = 'https://www.fortinos.ca/home-and-living/c/27986'
+    base_url = 'https://www.fortinos.ca/home-and-living/household-cleaning-products/c/28011'
     all_links = get_all_links(driver, base_url)
+
     for link in all_links:
         print(link)
 finally:
@@ -53,4 +61,8 @@ https://www.fortinos.ca/home-and-living/c/27986
 https://www.fortinos.ca/baby/c/27987
 https://www.fortinos.ca/pet-supplies/c/27988
 https://www.fortinos.ca/health-beauty/c/27994
+https://www.fortinos.ca/toys-games-hobbies/c/27990
+https://www.fortinos.ca/computers-electronics/c/27992
+https://www.fortinos.ca/lawn-garden-patio/c/27995
+https://www.fortinos.ca/home-and-living/household-cleaning-products/c/28011
 """
