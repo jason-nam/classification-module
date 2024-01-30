e17a0fedcb1fddc3013805f771093bd8dc3396d6
jason-nam <namy2@mcmaster.ca>
2024-01-29 22:40:58 -0500

    created new file for fortinos web scraping purposes

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,5 @@
+from src.products_scraper import ProductsScraper
+from src.fortinos_scraper import FortinosProductsScraper
+
 from src.workflows import execute_workflow
 from src.web_crawler import get_all_links
